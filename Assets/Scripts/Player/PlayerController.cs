84f4bafa3323ad9f88a0f7a71c66da95335f7aa6
dev06 <devanpatel13@gmail.com>
2016-08-28 15:22:56 -0400

    Updated bot and PC

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,26 +5,18 @@ public class PlayerController : Mob
 {
 
 
-    void Start()
-    {
-        Health = 100;
-       
-    }
+	void Start()
+	{
+		Health = 100;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-
-<<<<<<< HEAD
-	} void OnCollisionEnter(Collision collision) {
-		Debug.Log("hy8it");
 	}
-=======
 
-    }
+	// Update is called once per frame
+	void Update()
+	{
 
->>>>>>> b450b2d8821aa3ebfb560d7febebe756d2f735b4
+
+	}
 
 
 
